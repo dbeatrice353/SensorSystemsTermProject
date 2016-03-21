497e5efe6acb79d2df5dec6719259f51a7a4d66d
David Beatrice <dbeatrice353@gmail.com>
2016-03-20 21:32:18 -0400

    db things

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
+/*
+David Beatrice
+
+Sensor Systems Project - the cloud app
+
+The app should handle 3 cases:
+
+1) receive hello messages from sensor nodes (POST)
+2) receive alerts from nodes (POST)
+3) Respond to requests for the web interface (GET)
+4) Respond to data update requests from the web interface (GET)
+5) Respond to a request for a browser-based fake node for testing purposes (GET)
+
+*/
+
+
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
+// static files (js)
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
+// views
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+// Respond to requests for the web interface (GET)
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// Respond to a request for a browser-based fake node
+// for testing purposes (GET)
+app.get('/testnode', function(request, response) {
+  response.render('pages/testnode');
 });
 
+// Receive "hello" messages from nodes (POST)
+app.post('/hello', function (req, res) {
+});
 
+// Receive alerts from nodes (POST)
+app.post('/alert', function (req, res) {
+});
+
+// Respond to data update requests from the web interface (GET)
+app.get('/data', function (req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM sensor_node', function(err, nodes) {
+      done();
+      if (err){
+        console.error(err); res.send("Error " + err);
+      } else {
+        // nesting a second query in the callback because that's how this works for whatever reason...
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+          client.query('SELECT * FROM alert', function(err, alerts) {
+            done();
+            if (err){
+              console.error(err); res.send("Error " + err);
+            } else {
+              res.send(JSON.stringify({'nodes':nodes.rows,'alerts':alerts.rows}));
+            }
+          });
+        });
+      }
+    });
+  });
+});
+
+// test the db connection
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM sensor_node', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port', app.get('port'),'...');
+});
---
+++ public/js/testnode.js
@@ -0,0 +1,37 @@
+/*
+David Beatrice
+
+This js simuates the network behavior of a sensor node, enabling us to
+test the respective server-side code.
+
+The code does 2 things:
+
+1) send periodic "hello" messages back to the server, indicated that this
+   node is still alive.
+
+2) send "alert" messages to the server whenever the sensor has been triggered.
+   In this case, a button on the html page is used to initiate the "alert"
+   message.
+
+*/
+
+// send an alert back to the server
+function send_alert(){
+  console.log(Date() + " *alert sent*");
+}
+
+// bind the alert dispacher "send_alert" to the button
+function bind_alert_to_button(){
+  document.getElementById("alert").onclick = send_alert;
+}
+
+// send periodic "hello" messages back to the server
+function say_hello(){
+  var hello_period = 3; // seconds between each "hello"
+  setInterval(function(){
+      console.log(Date() + " *hello sent*");
+    }, hello_period*1000);
+}
+
+bind_alert_to_button();
+say_hello();
